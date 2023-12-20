a884bef088fa3e914d01fa815f22efa0cec93af3
Ajaykumars7 <ajaykumars.nevgiv@gmail.com>
2023-12-20 20:06:00 +0530

    arthimatic expression programming

--- Java/Learning/hello.java
+++ Java/Learning/hello.java
@@ -1,12 +1,24 @@
 
 
-// Add two numbers
+// arthimetic expression
 class hello{
     public static void main(String[] args){
-        int a = 12, b = 13;
-        System.out.println(a + b);
+        int a = 45, b = 45, d = 4398, e = 758 , c= 45; 
+        float c = 78347.077;
+        int x = a + c;
+        System.out.println(x);
     }
 }
+
+
+
+// Add two numbers
+// class hello{
+//     public static void main(String[] args){
+//         int a = 12, b = 13;
+//         System.out.println(a + b);
+//     }
+// }
 
 
 
