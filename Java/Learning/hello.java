f550bcc943256240dfdeac30c1572a46fb5fc824
Ajaykumars7 <ajaykumars.nevgiv@gmail.com>
2023-12-20 19:53:46 +0530

    add two numbers executed

--- Java/Learning/hello.java
+++ Java/Learning/hello.java
@@ -1,10 +1,10 @@
 
 
 // Add two numbers
-class Hello{
+class hello{
     public static void main(String[] args){
         int a = 12, b = 13;
-        System.out.println("a + b");
+        System.out.println(a + b);
     }
 }
 
