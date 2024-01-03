36a635e8efa16c9c85d2ae0c76bf319fd6a5ce31
Ajay <154252591+ajaykumars7@users.noreply.github.com>
2024-01-03 00:13:57 +0000

    java ifelse statement practice

---
+++ Java/Learning/ifelse.java
@@ -0,0 +1,11 @@
+class ifelse{
+    public static void main(String agrs[]){
+    int a = 14;
+    if(a>=18)
+    {
+        System.out.println("ok to Vote");
+    }else{
+        System.out.println("not ok to vote");
+    }
+    }
+}
