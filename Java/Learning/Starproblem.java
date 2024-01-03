2e2d0ac7f75a784808db542ca5d5c228b8d13e1f
Ajaykumars7 <ajaykumars.nevgiv@gmail.com>
2024-01-03 12:08:01 +0530

    star problem practice - using for loop

---
+++ Java/Learning/Starproblem.java
@@ -0,0 +1,12 @@
+class Starproblem{
+    public static void main(String[] args){
+        System.out.println("D");
+        for (int i = 1; i < 3; i++){
+            System.out.println("a");
+               for (int j = 1; j<5; j++){
+                System.out.print("b");
+                }
+        }
+        System.out.println("c");
+    }
+}
