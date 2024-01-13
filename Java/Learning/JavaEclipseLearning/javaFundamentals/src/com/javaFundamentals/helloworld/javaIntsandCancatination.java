2a0b9456d72be4bf5cab0a54423a88ca114eab09
Ajaykumars7 <ajaykumars.nevgiv@gmail.com>
2024-01-13 19:41:51 +0530

    java ints and concatiation

---
+++ Java/Learning/JavaEclipseLearning/javaFundamentals/src/com/javaFundamentals/helloworld/javaIntsandCancatination.java
@@ -0,0 +1,15 @@
+package com.javaFundamentals.helloworld;
+
+public class javaIntsandCancatination {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String FirstName = "Ajay";
+		String lastName = "Kumar";
+				
+				
+		int number = 11;
+		System.out.println("My Name is " + FirstName + lastName + " and i Has " + number + " oranges");
+	}
+
+}
