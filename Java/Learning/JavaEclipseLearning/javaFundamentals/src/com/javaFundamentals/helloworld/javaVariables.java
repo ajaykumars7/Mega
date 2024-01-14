a122f0241725a85113e2d5e8cdda9062163beccf
Ajaykumars7 <ajaykumars.nevgiv@gmail.com>
2024-01-14 08:13:28 +0530

    java variables

---
+++ Java/Learning/JavaEclipseLearning/javaFundamentals/src/com/javaFundamentals/helloworld/javaVariables.java
@@ -0,0 +1,39 @@
+package com.javaFundamentals.helloworld;
+
+public class javaVariables {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+//		integer
+		int myInt = 62338;
+//		byte -> smaller number compare to int
+		byte myByte = 127;
+//		short -> 2x byte
+		Short myShort = 128;
+//		float 1.34 -> fraction but bit smaller number, at last f must be there wise
+//		compiler will confuse with double and float
+		float myFloat = 1.34f;
+//		double -> its a fraction but bigger number than float
+		double myDouble = 12121.89;
+//		Char -> single letter or number 
+		char myChar = 'A';
+//		String -> More than one char
+		String myString = "Ajay";
+//		boolean -> boolean value takes only 1 or 0
+		boolean myBool = true;
+
+		
+		
+		System.out.println(myInt);
+		System.out.println(myFloat);
+		System.out.println(myShort);
+		System.out.println(myByte);
+		System.out.println(myDouble);
+		System.out.println(myString);
+		System.out.println(myChar);
+		System.out.println(myBool);
+		
+	}
+
+}
--- Java/Learning/JavaEclipseLearning/javaFundamentals/src/com/javaFundamentals/helloworld/javacancatandinteger.java
+++ Java/Learning/JavaEclipseLearning/javaFundamentals/src/com/javaFundamentals/helloworld/javacancatandinteger.java
@@ -1,9 +1,10 @@
 package com.javaFundamentals.helloworld;
 
-public class javaIntsandCancatination {
+public class javacancatandinteger {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+
 		String FirstName = "Ajay";
 		String lastName = "Kumar";
 				
