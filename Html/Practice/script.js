af1a6f5c1fd6b26d4d37078f389789979631efff
Ajaykumars7 <ajaykumars.nevgiv@gmail.com>
2024-04-14 17:59:36 +0530

    java script basic challenges

---
+++ Html/Practice/script.js
@@ -0,0 +1,9 @@
+
+let firstname = 'ajay';
+document.querySelector(".withname").innerHTML = firstname;
+// alert(' 🎆🎇🎉🎊🥳 Welcome to page')
+let total = 0;
+document.querySelector('.showcase').innerHTML = `the total value is ${total}`
+
+
+
