fe60a3cee44d60e5286ba3b844debb94ab72a0df
Ajaykumars7 <ajaykumars.nevgiv@gmail.com>
2024-06-06 19:42:32 +0530

    java basics and variables

--- Java/Learning/Javalearning/src/trial.java
+++
@@ -1,3 +0,0 @@
-public class trial {
-    System.out.print("hhhhhhh");
-}
---
+++ Java/Learning/Javalearning/src/variables.java
@@ -0,0 +1,27 @@
+class variables{
+    public static void main(String[] args){
+//        Integer
+        int a = 10;
+        System.out.println(a);
+        a = 20;
+        System.out.println(a);
+
+//        float
+        float b = 2.45f;
+        System.out.println(b);
+        double c = 2.45d;
+        System.out.println(c + "It is a double");
+
+//        character and string
+        char d = 'a';
+        System.out.println(d);
+
+        String e = "ajay";
+        System.out.println(e);
+
+
+//        Boolean
+        boolean isadult = true;
+        System.out.println(isadult);
+    }
+}
