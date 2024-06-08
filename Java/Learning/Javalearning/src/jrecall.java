b767d7b3e96803ec30a0e732d25bf305f6bbe9fd
Ajaykumars7 <ajaykumars.nevgiv@gmail.com>
2024-06-08 20:03:58 +0530

    java exercises - setup files and java basics

---
+++ Java/Learning/Javalearning/src/jrecall.java
@@ -0,0 +1,8 @@
+class jrecall{
+    public static void main(String[] args){
+        int a = 25;
+        float b = 30.0001f;
+        double c = 33.3d;
+        System.out.println(a + b + c);
+    }
+}
