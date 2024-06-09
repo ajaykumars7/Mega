93b807d40ed589f5163c9b3d3237843ca1add054
Ajaykumars7 <ajaykumars.nevgiv@gmail.com>
2024-06-10 04:14:06 +0530

    java recalling previous topics

--- Java/Learning/Javalearning/src/jrecall.java
+++ Java/Learning/Javalearning/src/jrecall.java
@@ -3,6 +3,9 @@ class jrecall{
         int a = 25;
         float b = 30.0001f;
         double c = 33.3d;
-        System.out.println(a + b + c);
+        String d = "ajay";
+        char e = 'A';
+
+        System.out.println(a + b + c +"\n" +  d +"\n" + e);
     }
 }
