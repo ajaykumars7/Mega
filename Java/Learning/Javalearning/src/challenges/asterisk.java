364af8e5f4054d6091465ac4b68a5af384c52ca6
Ajaykumars7 <ajaykumars.nevgiv@gmail.com>
2024-06-10 04:41:39 +0530

    asterisk challege

--- Java/Learning/Javalearning/src/challenges/asterisk.java
+++ Java/Learning/Javalearning/src/challenges/asterisk.java
@@ -2,6 +2,12 @@ package challenges;
 
 class asterisk{
     public static void main(String[] args){
-        System.out.println("hi");
+
+        System.out.println("* * * * *\n* * * *\n* * *\n* *\n*\n\n");  //        reverse right hal pyramid
+        System.out.println("*\n* *\n* * *\n* * * *\n* * * * *\n\n");  //        right half pyramid
+        System.out.println("         *\n       * *\n     * * *\n   * * * *\n * * * * *\n\n");  //        left half pyramid
+
+
+
     }
 }
