876a3286aca6421eb9559e5b32342f3cab5f1c25
Ajaykumars7 <ajaykumars.nevgiv@gmail.com>
2024-06-14 11:57:11 +0530

    logical operator challenges

--- Java/Learning/Javalearning/src/challenges/LogicalOperator.java
+++ Java/Learning/Javalearning/src/challenges/LogicalOperator.java
@@ -28,10 +28,13 @@ class LogicalOperator{
         int num1 = input.nextInt();
         int num2 = input.nextInt();
         int num3 = input.nextInt();
-        if (num1 < num2 & num3 < num2){
-            System.out.println("num2 is greater" + num2);
-        } else if (num) {
-
+        System.out.println(num1 + " " + num2 + " " + num3);
+        if(num1 >= num2 && num1 >= num3){
+            System.out.println(num1 + " is greatest");
+        } else if (num2 >= num1 && num2 >= num3) {
+            System.out.println(num2 + " is greatest");
+        }else{
+            System.out.println(num3 + " is greatest");
         }
     }
 }
