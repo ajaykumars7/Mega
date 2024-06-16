70fe47d9ca899d270613edef5a103a67bc9b2957
Ajaykumars7 <ajaykumars.nevgiv@gmail.com>
2024-06-16 16:06:57 +0530

    methods and operator using together

---
+++ Java/Learning/Javalearning/src/Exercises/MethodsOperators.java
@@ -0,0 +1,13 @@
+package  Exercises;
+//import java.util.Scanner;
+
+class MethodsOperators{
+    public static int additions(int a, int b){
+        int result  = a + b;
+        return result;
+    }
+    public static void main(String[] args) {
+        System.out.println(additions(2,3));
+    }
+}
+
