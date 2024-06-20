01eef5c7f2948f2a8788de5f58d5c9eaf10c368e
Ajaykumars7 <ajaykumars.nevgiv@gmail.com>
2024-06-20 19:52:00 +0530

    Arrays in java

---
+++ Java/Learning/Javalearning/src/Exercises/arrays.java
@@ -0,0 +1,15 @@
+package Exercises;
+
+
+class arrays{
+    public static void main(String[] args) {
+        int[] Numbers = new int[5];
+
+        int[] marks = {71, 77, 80, 99, 100};
+        int i = 0;
+        while(marks.length > i){
+            System.out.println(marks[i]);
+            i++;
+        }
+    }
+}
