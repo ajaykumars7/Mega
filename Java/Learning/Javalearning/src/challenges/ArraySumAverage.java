08618949e355554fe5882727643edf50c972307f
Ajaykumars7 <ajaykumars.nevgiv@gmail.com>
2024-06-24 10:42:11 +0530

    array sum average

---
+++ Java/Learning/Javalearning/src/challenges/ArraySumAverage.java
@@ -0,0 +1,26 @@
+package challenges;
+import java.util.Scanner;
+
+public class ArraySumAverage {
+    public static void main(String[] args) {
+        System.out.println("welcome to array sum and average");
+        int[] numArray = JavaUtility.inputArray();
+        int sum = sum(numArray);
+        float average = average(numArray);
+        System.out.println(sum);
+        System.out.println(average);
+    }
+    public static int sum(int[] numArray){
+        int sum = 0, i = 0;
+        while(i < numArray.length){
+            sum += numArray[i];
+            i++;
+        }
+        return sum;
+    }
+    public static float average(int[] numArray){
+        int sum = sum(numArray);
+        return (float) (sum/ numArray.length);
+    }
+
+}
