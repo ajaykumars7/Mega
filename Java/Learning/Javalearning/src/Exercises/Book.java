a650986666cf604668079c94c46fade898cf684e
Ajaykumars7 <ajaykumars.nevgiv@gmail.com>
2024-07-03 12:17:46 +0530

    oops basics

---
+++ Java/Learning/Javalearning/src/Exercises/Book.java
@@ -0,0 +1,17 @@
+package Exercises;
+
+public class Book {
+    String title;
+    String Author;
+    long isbn;
+    static int totalBooks;
+    public void borrowBook(){
+
+    }
+    public void returnBook(){
+
+    }
+    public static int getTotalBooks(){
+
+    }
+}
