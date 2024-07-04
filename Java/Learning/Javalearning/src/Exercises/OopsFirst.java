31749281c5be6a46d6989d499f17c325b8565e23
Ajaykumars7 <ajaykumars.nevgiv@gmail.com>
2024-07-04 11:49:27 +0530

    oops fundamentals - objects and classes

--- Java/Learning/Javalearning/src/Exercises/OopsFirst.java
+++ Java/Learning/Javalearning/src/Exercises/OopsFirst.java
@@ -1,4 +1,26 @@
 package Exercises;
 
 public class OopsFirst {
+    public static class car{
+        int noOfWheels;
+        String title;
+
+        void engineStart(){
+            System.out.println("Engine Started");
+        }
+        void engineStop(){
+            System.out.println("Engine Stopped");
+        }
+
+    }
+
+    public static void main(String[] args) {
+        car myCar = new car();
+        myCar.noOfWheels = 4;
+        myCar.title = "Audi";
+        myCar.engineStart();
+        myCar.engineStop();
+        System.out.println("your car as " + myCar.noOfWheels + "Wheels");
+    }
+
 }
