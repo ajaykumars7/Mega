ed38eeb984a619673780056d6e11a74e14ae35ea
Ajaykumars7 <ajaykumars.nevgiv@gmail.com>
2024-07-09 19:29:05 +0530

    Switch statement - comprehensive method

--- Java/Learning/Javalearning/src/Exercises/SwitchExercise.java
+++ Java/Learning/Javalearning/src/Exercises/SwitchExercise.java
@@ -4,18 +4,29 @@ public class SwitchExercise {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int day = input.nextInt();
+        oldSwitch(day);
+        newSwitch(day);
 
+    }
+
+    static void newSwitch(int day){
+        String daystr = switch (day){
+            case 1 -> "monday";
+            case 2 -> "tuesday";
+            case 3 -> "wednesday";
+            default -> "invalid";
+        };
+        System.out.println(daystr);
+    }
+    static void oldSwitch(int day){
         switch (day){
             case 1 : System.out.println("monday");
-            break;
+                break;
             case 2 : System.out.println("Tuesday");
-            break;
+                break;
             case 3 : System.out.println("Wednesday");
-            break;
-            default:
-                System.out.println("invalid");
-
+                break;
+            default: System.out.println("invalid");
         }
-
     }
 }
