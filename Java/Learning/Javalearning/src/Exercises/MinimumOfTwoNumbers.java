918600ceb54f12b77ac90adf35ce7cfc33f54686
Ajaykumars7 <ajaykumars.nevgiv@gmail.com>
2024-07-12 21:50:14 +0530

    absolute of a number

--- Java/Learning/Javalearning/src/Exercises/MinimumOfTwoNumbers.java
+++ Java/Learning/Javalearning/src/Exercises/MinimumOfTwoNumbers.java
@@ -6,15 +6,17 @@ public class MinimumOfTwoNumbers {
         Scanner input = new Scanner(System.in);
         System.out.print("enter 1st number : ");
         int Num1 = input.nextInt();
-        System.out.print("enter 2st number : ");
-        int Num2 = input.nextInt();
+//        System.out.print("enter 2st number : ");
+//        int Num2 = input.nextInt();
+        int result = Num1 >= 0 ? Num1 : -Num1;
+        System.out.println(result);
     }
-    public int min(int Num1, int Num2){
+//    public int min(int Num1, int Num2){
 //        if(num1 < num2){
 //            return num2;
 //        }else{
 //            return num1;
 //        }
-        return Num1 < Num2 : Num1 ? Num2;
-    }
+//        return Num1 < Num2 : Num1 ? Num2;
+//    }
 }
