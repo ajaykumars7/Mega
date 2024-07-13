0044a069e63f77b3aff0500ce9902dc4cf553024
Ajaykumars7 <ajaykumars.nevgiv@gmail.com>
2024-07-13 10:43:14 +0530

    reverse number using do while

---
+++ Java/Learning/Javalearning/src/challenges/ReverseNumberWhile.java
@@ -0,0 +1,9 @@
+package challenges;
+
+public class ReverseNumberWhile {
+    public static void main(String[] args) {
+        int num1 = JavaUtility.simpleNumber();
+        int num2 = JavaUtility.simpleNumber();
+
+    }
+}
