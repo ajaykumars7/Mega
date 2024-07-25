68191a39c753ec363d85c2509cce64c2a168e973
Ajaykumars7 <ajaykumars.nevgiv@gmail.com>
2024-07-25 12:53:38 +0530

    concat string

---
+++ Java/Learning/Javalearning/src/collectionExercise/ConcatString.java
@@ -0,0 +1,11 @@
+package collectionExercise;
+
+public class ConcatString {
+ public static String concatenate(String... strs){
+     StringBuilder sb = new StringBuilder();
+     for(String str: strs){
+         sb.append(str).append(" ");
+     }
+     return sb.toString();
+ }
+}
