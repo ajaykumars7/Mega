a003bf6a3b81a969ad57b7678c4344ba08510771
ajaykumars7 <ajaykumars.nevgiv@gmail.com>
2024-09-03 08:15:07 +0530

    add two numbers

--- Java/Practice/CoreJavaPractice/src/AddTwoNumbers.java
+++ Java/Practice/CoreJavaPractice/src/AddTwoNumbers.java
@@ -1,2 +1,8 @@
 public class AddTwoNumbers {
+    public static void main(String[] args) {
+        int n1 = 10;
+        int n2 = 20;
+
+        System.out.println("n1 + n2 = " + (n1 + n2));
+    }
 }
