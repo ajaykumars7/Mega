f8dd2c3b32fba23c14c2d1286018636e1392f30b
ajaykumars7 <ajaykumars.nevgiv@gmail.com>
2024-09-03 12:35:34 +0530

    variables problem practice

---
+++ Java/Practice/CoreJavaPractice/src/Datatypes.java
@@ -0,0 +1,99 @@
+public class Datatypes {
+    public static void main(String[] args) {
+//        System.out.println("Practice Data Types");
+
+
+        // Problem - 1
+//        Declare variables of different data types (int, double, char, boolean) and initialize them with values. Print them to the console.
+//        int n1 = 10;
+//        float n2 = 20;
+//        char letter = 'a';
+//        String name = "Ajay";
+//        boolean person = true;
+//
+//        System.out.println("int Number = " + n1 + "\n" + "float number = " + n2
+//                + "\n" + "Character letter = " + letter +"\n" + "String name = " + name +
+//                "\n" +"boolean person = " + person);
+
+
+//        Problem -2
+//        Write a program that swaps the values of two variables without using a third variable.
+//        int n1 = 10;
+//        int n2 = 23;
+//        n1 = n1 + n2; //33
+//        n2 = n1 - n2; //10
+//        n1 = n1 - n2; //23
+//        System.out.println("n1 = " + n1);
+//        System.out.println("n2 = " + n2);
+
+//        problem - 3
+//        Given two double values representing lengths of the sides of a rectangle, calculate and print the area and perimeter of the rectangle.
+
+//        double length = 24;
+//        double width = 32;
+//
+//        double area = length * width;
+//        double parameter = 2 * (length + width);
+//
+//        System.out.println("area = " + area);
+//        System.out.println("parameter  = " + parameter);
+
+
+//        float n1 = 10.2222f;
+//        int n2 = (int)n1;
+//
+//        System.out.println(n1);
+//        System.out.println(n2);
+
+
+//        String num = "123";
+//        int num1 = 12;
+//        int res = Integer.parseInt(num) - num1;
+//
+//        System.out.println(res);
+
+
+//        int num = 10;
+//        public void Sample(int a){
+//            int num = 23;
+//            System.out.println(num);
+//        }
+//        System.out.println(num);
+
+//        final float pi = 3.14f;
+//        float radius = 22.21f;
+//        double area = pi * Math.pow(radius, 2);
+//        System.out.println(area);
+
+
+//        int num1 = 43;
+//        int num2 = 23;
+//
+//        System.out.println("num1 + num2 = " + (num1 + num2));
+//        System.out.println("num1 - num2 = " + (num1 - num2));
+//        System.out.println("num1 * num2 = " + (num1 * num2));
+//        System.out.println("num1 / num2 = " + (num1 / num2));
+//        System.out.println("num1 % num2 = " + (num1 % num2));
+
+
+//        int num = 10; //13
+//        int num1 = num++; //11
+//        int num2 = num++ + ++num + num1++;  //34
+//        System.out.println(num);
+//        System.out.println(num1);
+//        System.out.println(num2);
+
+
+//        int num = 11;
+//
+//        public void checkEvenOdd(int num){
+//           int res = (num % 2);
+//                if (res == 1){
+//                    System.out.println("odd");
+//                }else {
+//                    System.out.println("even");
+//                }
+//        }
+    }
+
+}
