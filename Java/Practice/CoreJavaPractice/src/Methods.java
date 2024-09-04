6b4bbe5675c4f0ed725e850cd31cc5a451f2bb2c
ajaykumars7 <ajaykumars.nevgiv@gmail.com>
2024-09-04 09:59:41 +0530

    conditional statement

---
+++ Java/Practice/CoreJavaPractice/src/Methods.java
@@ -0,0 +1,12 @@
+public class Methods {
+    public static void main(String[] args) {
+
+//        int a = 10;
+//        int b = 34;
+
+//        public int addition(int a, int b){
+//            int sum = a + b;
+//            return sum;
+//        }
+    }
+}
