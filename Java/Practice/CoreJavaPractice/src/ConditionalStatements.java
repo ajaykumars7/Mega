292729c9e6304d45c30f03128085ce53bddc70fd
ajaykumars7 <ajaykumars.nevgiv@gmail.com>
2024-09-04 09:59:14 +0530

    conditional statement

---
+++ Java/Practice/CoreJavaPractice/src/ConditionalStatements.java
@@ -0,0 +1,23 @@
+public class ConditionalStatements {
+    public static void main(String[] args) {
+
+//        int year = 780;
+//        String isLeapYear = ((year % 400 == 0) || (year % 100 != 0) && (year % 4 == 0))?
+//                "leap lear":"not a leap year";
+//
+//
+//        System.out.println(isLeapYear);
+
+
+
+        /*
+        int a = 2; // 10
+        int b = 4; // 100
+
+        System.out.println("2 >> 4 = " + (a >> b)); // 0010. -. 000 -> 0
+        System.out.println("2 << 4 = " + (a << b)); // 0010. -> 00100001 -> 32
+         */
+
+
+    }
+}
