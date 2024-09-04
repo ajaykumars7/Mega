b7f9eb576fe2dd5817c4ab6b0fa6ea884fee12c4
ajaykumars7 <ajaykumars.nevgiv@gmail.com>
2024-09-04 16:37:24 +0530

    implicit and explicit type casting in kodnest

---
+++ Java/Practice/CoreJavaPractice/src/KodnestPrograms.java
@@ -0,0 +1,63 @@
+public class KodnestPrograms {
+    public static void main(String[] args) {
+
+
+
+
+
+
+
+
+
+        //TYPE CASTING PROBLEM
+
+        int a = 10;
+        int b = 4;
+
+        int res = a / b; // 2
+        double res1 = a / b; // 2.0
+        double res2 = a /(double) b; //2.5
+
+        System.out.println("res = " + res);
+        System.out.println("res1 = " + res1);
+        System.out.println("res2 = " + res2);
+
+
+
+        //IMPLICIT & EXPLICIT
+
+//        int a = 10;
+//        double b = a; //implicit type casting
+//
+//        System.out.println("INT a = " + a);
+//        System.out.println("Double b = " + b);
+//
+//
+//        double x = 23.326782;
+//        int y = (int)x; // explicit type casting
+//        System.out.println("Double x = " + x);
+//        System.out.println("Double y = " + y);
+
+//        DATATYPES
+//        byte roll = 1;
+//        short pin = 3232;
+//        int ctc = 500000; // 5LPA
+//        long mblNo = 82749728090L;
+//
+//        System.out.println("Roll = " + roll);
+//        System.out.println("pin = " + pin);
+//        System.out.println("CTC = " + ctc);
+//        System.out.println("mblNo = " + mblNo);
+
+
+
+//       BITWISE OPERATOR
+//        var num1 = 15;
+//        var num2 = 7;
+//
+//        System.out.println("num1 & num2 = " +( num1 & num2));
+//        System.out.println("num1 | num2 = " +( num1 | num2));
+//        System.out.println("num1 ^ num2 = " +( num1 ^ num2));
+
+            }
+}
