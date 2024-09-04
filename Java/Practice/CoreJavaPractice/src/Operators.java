1bf9a1365ec33275ec7ca618125a2744c5390c5f
ajaykumars7 <ajaykumars.nevgiv@gmail.com>
2024-09-04 10:00:07 +0530

    Operators Practice

---
+++ Java/Practice/CoreJavaPractice/src/Operators.java
@@ -0,0 +1,67 @@
+public class Operators {
+    public static void main(String[] args) {
+
+
+
+
+
+//        int a = 10;
+//        int b = 27;
+//        int c = 32;
+//
+//        int avg = (a+b+c)/3;
+//
+
+
+
+
+//        int a = 10;
+//        int b = 14;
+//
+//        if(a > b){
+//            System.out.println("a greater than b");
+//        } else if (a < b) {
+//            System.out.println("a less than b");
+//        }else {
+//            System.out.println("both are equal");
+//        }
+
+
+
+//        int a = 115;
+//        if(a <= 20 && a >= 10){
+//            System.out.println("it's in the range");
+//        }else {
+//            System.out.println("it's not in the range");
+//        }
+
+
+//        int a = 35;
+//
+//        if((a % 5 == 0) && (a % 7 == 0)){
+//            System.out.println("it's divisible by 5 and 7");
+//        }else{
+//            System.out.println("it's not divisible by 5 and 7");
+//        }
+
+
+//        int a = 15;
+//        int b = 10;
+
+//        System.out.println("a & b = " + (a & b));
+//        System.out.println("a | b = " + (a | b));
+//        System.out.println("a ^ b = " + (a ^ b));
+
+//        int a = 0;
+//        String result  = (a > 0) ? "Positive" : (a < 0) ? "Negative" : "zero";
+//        System.out.println(result);
+
+//        int sum = 0;
+//        for(int i = 1; i <= 100; i++){
+//          sum += i;
+//        }
+//
+//        System.out.println(sum);
+
+    }
+}
