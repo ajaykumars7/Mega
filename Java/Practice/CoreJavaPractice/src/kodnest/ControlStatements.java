c8ca3b7c2c235b4387404c2a0a8e84cb906a71e0
ajaykumars7 <ajaykumars.nevgiv@gmail.com>
2024-09-10 15:46:05 +0530

    build simple Calculator - kodnest practice

--- Java/Practice/CoreJavaPractice/src/kodnest/ControlStatements.java
+++ Java/Practice/CoreJavaPractice/src/kodnest/ControlStatements.java
@@ -3,26 +3,72 @@ package kodnest;
 import java.util.Scanner;
 
 public class ControlStatements {
+
+    public static void add(int a, int b){
+        int sum = a + b;
+        System.out.println(sum);
+    }
+    public static void sub(int a, int b){
+        int sub = a - b;
+        System.out.println(sub);
+    }
+    public static void mul(int a, int b){
+        int mul = a * b;
+        System.out.println(mul);
+    }
+    public static void div(int a, int b){
+        int div = a / b;
+        System.out.println(div);
+    }
+
+
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+
+
+        System.out.println("Select to perform a particular Operation");
+        System.out.println("enter 1-add, 2-sub, 3-mul, 4-div");
+        int operationNum = input.nextInt();
+
+
+
+        System.out.println("Enter the First number");
+        int num1 = input.nextInt();
+        System.out.println("Enter the Second number");
+        int num2 = input.nextInt();
+
+        if(operationNum == 1){
+            add(num1, num2);
+        } else if (operationNum == 2) {
+            sub(num1, num2);
+        }else if (operationNum == 3) {
+            mul(num1, num2);
+        }else if (operationNum == 4) {
+            div(num1, num2);
+        }else{
+            System.out.println("WRONG INPUT.....TRY AGAIN");
+        }
+
+
+
 
 
 
 
         //ELSE-IF BLOCK
-        System.out.println("Enter your marks");
-        int marks = input.nextInt();
-        if(marks >= 91){
-            System.out.println("get a Bike");
-        } else if (marks >= 81) {
-            System.out.println("get a laptop");
-        } else if (marks >= 71) {
-            System.out.println("get a mobile");
-        } else if (marks >= 41) {
-            System.out.println("get a cycle");
-        }else{
-            System.out.println("Get Out.....");
-        }
+//        System.out.println("Enter your marks");
+//        int marks = input.nextInt();
+//        if(marks >= 91){
+//            System.out.println("get a Bike");
+//        } else if (marks >= 81) {
+//            System.out.println("get a laptop");
+//        } else if (marks >= 71) {
+//            System.out.println("get a mobile");
+//        } else if (marks >= 41) {
+//            System.out.println("get a cycle");
+//        }else{
+//            System.out.println("Get Out.....");
+//        }
 
 
 //        TAKE 2 NUMBER AND GET WHICH ONE IS GREATER
