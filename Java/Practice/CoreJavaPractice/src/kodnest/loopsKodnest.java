1269e48972fd3039ef0d861c88860646ab9b6af7
ajaykumars7 <ajaykumars.nevgiv@gmail.com>
2024-09-12 14:31:01 +0530

    Loops - kodnest practice

---
+++ Java/Practice/CoreJavaPractice/src/kodnest/loopsKodnest.java
@@ -0,0 +1,14 @@
+package kodnest;
+
+public class loopsKodnest {
+    public static void main(String[] args) {
+//        System.out.println("Hello Kodnest");
+//        System.out.println("Hello Kodnest");
+//        System.out.println("Hello Kodnest");
+//        System.out.println("Hello Kodnest");
+
+        for(int i = 0; i < 5; i++){
+            System.out.println("Hello Kodnest");
+        }
+    }
+}
