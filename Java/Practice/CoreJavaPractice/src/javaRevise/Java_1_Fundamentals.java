c29864974e2fd6abddfa12c57b59e90832230984
ajaykumars7 <ajaykumars.nevgiv@gmail.com>
2024-09-25 05:23:46 +0530

    java fundamentals revise

---
+++ Java/Practice/CoreJavaPractice/src/javaRevise/Java_1_Fundamentals.java
@@ -0,0 +1,18 @@
+package javaRevise;
+
+public class Java_1_Fundamentals {
+    public static void main(String[] args) {
+
+//        System.out.println("*****\n****\n***\n**\n*");
+//        System.out.println();
+//        System.out.println("*\n**\n***\n****\n*****");
+//        System.out.println();
+//        System.out.println("    *\n   **\n  ***\n ****\n*****");
+
+
+//        System.out.println("Subscribe");
+
+
+        //        System.out.println("Welcome");
+    }
+}
